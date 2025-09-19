9b16c71aefde5ac31124bdd1271dce1d3e78a202
Bartele <kamiro1234567890@wp.pl>
2025-09-19 22:17:58 +0200

    aaa

---
+++ index.js
@@ -0,0 +1,21 @@
+let nazwaUzytkownika = 'jan_kowalski'; // String poniewaz jest ciagiem znakow
+
+let wiekUzytkownika = 28;             // Number poniewaz jest liczba
+
+let czyZgadzaSieNaNewslettery = true;// Boolean  poniewaz ma wartosc logiczna true/false
+
+let numerTelefonu = '+48123456789';    // String poniewaz jest ciagiem znakow
+
+let wybranaPlacNarodowosci;                   // Undefined poniewaz jest wartoscia domyslna
+
+let preferowanyJezyk = null;             // Null  poniewaz nie  ma wartosci
+
+let iloscZalogowan = 0;                // Number  poniewaz jest liczba
+
+let czyPotwierdzilEmail = false;       // Boolean poniewaz ma wartosc logiczna true/false
+
+//poprawki wydaja mi sie zbedne (jedynie co moge zmienic to let na const w niektorych przypadkach)
+
+// wybranaPlacNarodowosci czyli  undefined to wartosc domysla ktora jest przypisywana automatycznie
+
+//preferowanyJezyk czyli null ktora uzytkownik sam swiadomie przypisuje uzywamy jej gdy chcemy 'wyczyscic' zmienna lub oznaczyc brak wartosci
